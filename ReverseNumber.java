cdba99b6b2333fb1ebc3e2d0ede7e8ddd2784d7f
jeniferamal19 <166788307+jeniferamal19@users.noreply.github.com>
2026-07-11 04:59:21 +0530

    Updated Reverse Number program

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,14 +1,14 @@
 package sample.java;
-class Practice{
+class ReverseNumber{
 		public static void main(String[] args) {
 		int rev=0;
 		for(int num=12345;num!=0;num=num/10) {
 			
 			int digit=num%10;
 			rev=rev*10+digit;
-			System.out.println(rev);
+			
 		}
-		
+		System.out.println(rev);
 		}
 }
 		
